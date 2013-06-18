925e7ad8ed95eee82789dec782915baae44847a0
Bastien Scher <bastien0705@gmail.com>
2013-06-18 23:08:24 +0200

    Add the led_pattern function

--- libled.s
+++ libled.s
@@ -5,7 +5,14 @@
 .globl led_mode_write
 .globl led_on
 .globl led_off
+.globl led_status
 .globl led_pattern
+
+led_mode_read:
+	ldr    r0, =gpio_ctrl
+	mov    r1, #0
+	str    r1, [r0, #0x04]
+	bx     lr
 
 led_mode_write:
 	ldr    r0, =gpio_ctrl
@@ -28,15 +35,68 @@ led_off:
 	str    r1, [r0, #0x1c]
 	bx     lr
 
-# Affiche une séquence de positions de led
-# r0: motif de bits, 0 pour éteint et 1 pour allumé
-# r1: nombre de bits utilisés (de poids faible à poids fort)
-# r2: temps à attendre
-# r3: si la séquence doit se répéter indéfiniment
+led_status:
+	ldr    r1, =gpio_ctrl
+	ldr    r0, [r1, #0x34]
+	mvn    r0, r0
+	bx     lr
+
+@ Affiche une séquence de positions de led
+@ r0: motif de bits, 0 pour éteint et 1 pour allumé
+@ r1: nombre de bits utilisés (de poids faible à poids fort)
+@ r2: temps d'attente entre chaque changement
+@ r3: si la séquence doit se répéter indéfiniment (non-implémenté)
 led_pattern:
 	push   {lr}
+	mov    r4, r0
+	mov    r5, r1
+	mov    r6, r2
+	mov    r7, r3
+	
+	@ Bloque r5 à 32
+	cmp    r5, #32
+	movhi  r5, #32
+
+	@ Récupère l'état actuel de la LED
+	@bl     led_mode_read
+	@bl     led_status
+	@mov    r8, r0
 	bl     led_mode_write
 
-#TODO
+	led_pattern$loop:
+	mov    r10, #1  @ masque roulant
+	mov    r11, r5  @ itérateur
 
-	pop   {pc}
+	led_pattern$nextbit:
+	ands   r9, r4, r10
+	beq    led_pattern$off
+
+	led_pattern$on:
+	@cmp    r8, #0
+	@bleq   led_on
+	bl      led_on @debug
+	b       led_pattern$endbit
+
+	led_pattern$off:
+	@cmp    r8, #0
+	bl      led_off @debug
+	blne    led_off
+
+	led_pattern$endbit:
+	@mov    r8, r9
+	subs   r11, r11, #1
+	beq    led_pattern$end
+	mov    r0, r6
+	bl     _wait
+	lsl    r10, #1
+	b      led_pattern$nextbit
+
+	led_pattern$end:
+	pop    {pc}
+
+@ Attend un temps donné dans r0
+_wait:
+	cmp    r0, #0
+	bxeq   lr
+	sub    r0, r0, #1
+	b       _wait
--- main.s
+++ main.s
@@ -1,11 +1,18 @@
+.section .rodata
+	.equ pattern, 0b0101001100110101
+	.equ delay, 0x3f0000
+
 .section .text
 .globl _start
-.extern led_on
+.extern led_pattern
 _start:
 	mov    sp, #0x10000
 
-	bl     led_mode_write
-	bl     led_on
+	ldr    r0, =pattern
+	mov    r1, #16
+	ldr    r2, =delay
+	mov    r3, #0
+	bl     led_pattern
 
 loop:
 	b      loop
