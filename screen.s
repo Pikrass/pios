8e452c9660fb1493510b26efbc37d0c2ea5b4dda
Bastien Scher <bastien0705@gmail.com>
2013-07-07 13:23:46 +0200

    Add memory barriers around mailbox calls

--- screen.s
+++ screen.s
@@ -16,26 +16,35 @@ _fb_info:
 	.int 0 // fb_addr
 	.int 0 // fb_size
 
+.macro barrier
+	mcr    p15, 0, r3, c7, c10, 5
+.endm
+
 /* Request a framebuffer from the GPU
  * r0: width
  * r1: height
  * r2: depth (bits/pixel)
  */
 fb_request:
-	push   {r4, lr}
+	push   {r4, r5, lr}
 	adr    r4, _fb_info
 	str    r0, [r4]
 	str    r1, [r4, #4]
 	str    r0, [r4, #8]
 	str    r1, [r4, #12]
 	str    r2, [r4, #20]
+	mov    r5, #0
 
 	orr    r0, r4, #1
+	barrier
 	bl     mailbox_write
+	barrier
 
 	mov    r0, #1
 	bl     mailbox_read
+	barrier
 	cmp    r0, #0
+	movne  r0, #1
 	popne  {r4, pc}
 
 	fb_request$wait_pointer:
@@ -43,4 +52,4 @@ fb_request:
 	cmp    r0, #0
 	beq    fb_request$wait_pointer
 
-	pop    {r4, pc}
+	pop    {r4, r5, pc}
