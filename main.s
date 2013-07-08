84160e1d9167f21047722e2d0a46816422cb0251
Bastien Scher <bastien0705@gmail.com>
2013-07-08 18:20:08 +0200

    Print some example numbers and the address of the framebuffer

--- main.s
+++ main.s
@@ -10,7 +10,10 @@
 	welcome: .ascii "Welcome to Pios, the little program wishing to become a "
 	         .ascii "full operating system some day. But for now it barely "
 	         .ascii "knows how to print a string, so... here it is!\n\n"
-	         .asciz "... You can turn off the Pi now."
+	         .ascii "This must read 0x1234abcd : 0x%x\n"
+	         .ascii "This must read 0x5678 : 0x%x\n"
+	         .ascii "This must read 0xbeef : 0x%x\n\n"
+	         .asciz "Framebuffer is at : 0x%x"
 	logo:    .incbin "logo.bin"
 
 .section .text
@@ -29,6 +32,7 @@
 
 _start:
 	mov    sp, #0x8000
+	sub    sp, #24
 
 	mov    r0, #1024
 	mov    r1, #768
@@ -36,6 +40,7 @@ _start:
 	bl     fb_request
 	cmp    r0, #0
 	beq    error
+	str    r0, [sp]
 
 	mov    r1, #1024
 	mov    r2, #768
@@ -72,15 +77,21 @@ _start:
 		cmp      r1, r5
 		bne      line
 
-	sub    sp, sp, #20
 	mov    r1, #100
 	mov    r2, #20
-	mov    r3, sp
+	add    r3, sp, #4
 	bl     term_create
 
-	mov    r0, sp
+	add    r0, sp, #4
+	ldr    r1, [sp]
+	push   {r1}
+	ldr    r1, =0xbeef
+	push   {r1}
 	ldr    r1, =[welcome]
+	ldr    r2, =0x1234abcd
+	ldr    r3, =0x5678
 	bl     term_printf
+	add    sp, sp, #8
 			
 	ldr    r0, =pattern_ok
 	mov    r1, #8
