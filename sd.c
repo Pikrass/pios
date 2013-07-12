649c72d25942bd5e4868a2a16005103ce46dc1c9
Bastien Scher <bastien0705@gmail.com>
2013-07-12 03:18:34 +0200

    Parse card's CSD

--- sd.c
+++ sd.c
@@ -10,7 +10,8 @@ void idle(int ms) {
 
 int sd_init(struct terminfo *term) {
 	int tmp, resp, powerup;
-	int card_type, rca, csd[4];
+	int card_type, rca, raw_csd[4];
+	struct csd csd;
 
 	term_printf(term, "SD_INIT:");
 
@@ -118,11 +119,49 @@ int sd_init(struct terminfo *term) {
 		term_printf(term, " cmd9_err");
 		return 0;
 	}
-	csd[0] = *RESP0;
-	csd[1] = *RESP1;
-	csd[2] = *RESP2;
-	csd[3] = *RESP3;
-	term_printf(term, " cmd9 csd=0x%x 0x%x 0x%x 0x%x", csd[0], csd[1], csd[2], csd[3]);
+	raw_csd[0] = *RESP0;
+	raw_csd[1] = *RESP1;
+	raw_csd[2] = *RESP2;
+	raw_csd[3] = *RESP3;
+	sd_parse_csd(raw_csd, &csd);
+	term_printf(term, " cmd9\n");
+	term_printf(term, "ff=0x%x, w_bl_len=0x%x, r2w=0x%x\n",
+			csd.file_format, csd.write_bl_len, csd.r2w_factor);
+	term_printf(term, "sector=0x%x, mult=0x%x, size=0x%x, dsr_imp=0x%x\n",
+			csd.sector_size, csd.c_size_mult, csd.c_size, csd.dsr_imp);
+	term_printf(term, "rd_bl_part=0x%x, r_bl_len=0x%x, ccc=0x%x, speed=0x%x\n",
+			csd.read_bl_partial, csd.read_bl_len, csd.ccc, csd.tran_speed);
 
 	return 1;
+}
+
+void sd_parse_csd(int raw[4], struct csd *csd) {
+	csd->file_format        = UNSTUFF_BITS(raw, 2, 2);
+	csd->tmp_write_protect  = UNSTUFF_BITS(raw, 4, 1);
+	csd->perm_write_protect = UNSTUFF_BITS(raw, 5, 1);
+	csd->copy               = UNSTUFF_BITS(raw, 6, 1);
+	csd->file_format_grp    = UNSTUFF_BITS(raw, 7, 1);
+	csd->write_bl_partial   = UNSTUFF_BITS(raw, 13, 1);
+	csd->write_bl_len       = UNSTUFF_BITS(raw, 14, 4);
+	csd->r2w_factor         = UNSTUFF_BITS(raw, 18, 3);
+	csd->wp_grp_enable      = UNSTUFF_BITS(raw, 23, 1);
+	csd->wp_grp_size        = UNSTUFF_BITS(raw, 24, 7);
+	csd->sector_size        = UNSTUFF_BITS(raw, 31, 7);
+	csd->erase_blk_en       = UNSTUFF_BITS(raw, 38, 1);
+	csd->c_size_mult        = UNSTUFF_BITS(raw, 39, 3);
+	csd->vdd_w_curr_max     = UNSTUFF_BITS(raw, 42, 3);
+	csd->vdd_w_curr_min     = UNSTUFF_BITS(raw, 45, 3);
+	csd->vdd_r_curr_max     = UNSTUFF_BITS(raw, 48, 3);
+	csd->vdd_r_curr_min     = UNSTUFF_BITS(raw, 51, 3);
+	csd->c_size             = UNSTUFF_BITS(raw, 54, 12);
+	csd->dsr_imp            = UNSTUFF_BITS(raw, 68, 1);
+	csd->read_blk_misalign  = UNSTUFF_BITS(raw, 69, 1);
+	csd->write_blk_misalign = UNSTUFF_BITS(raw, 70, 1);
+	csd->read_bl_partial    = UNSTUFF_BITS(raw, 71, 1);
+	csd->read_bl_len        = UNSTUFF_BITS(raw, 72, 4);
+	csd->ccc                = UNSTUFF_BITS(raw, 76, 12);
+	csd->tran_speed         = UNSTUFF_BITS(raw, 88, 8);
+	csd->nsac               = UNSTUFF_BITS(raw, 96, 8);
+	csd->taac               = UNSTUFF_BITS(raw, 104, 8);
+	csd->csd_structure      = UNSTUFF_BITS(raw, 118, 2);
 }
--- sd.h
+++ sd.h
@@ -90,8 +90,55 @@
 #define CMD_SEND_OP_COND       41
 #define CMD_APP                55
 
+#define UNSTUFF_BITS(base,start,size) \
+	({ \
+		const int __mask = (1 << (size)) - 1;            \
+		const int __shft = ((start) & 31);               \
+		int __res = (base[(start)/32] >> __shft);         \
+		if(__shft + (size) > 32)                         \
+			__res |= base[(start)/32+1] << (32 - __shft); \
+		__res & __mask;                                  \
+	})
+
+struct csd {
+	unsigned int                    : 2;
+	unsigned int file_format        : 2;
+	unsigned int tmp_write_protect  : 1;
+	unsigned int perm_write_protect : 1;
+	unsigned int copy               : 1;
+	unsigned int file_format_grp    : 1;
+	unsigned int                    : 5;
+	unsigned int write_bl_partial   : 1;
+	unsigned int write_bl_len       : 4;
+	unsigned int r2w_factor         : 3;
+	unsigned int                    : 2;
+	unsigned int wp_grp_enable      : 1;
+	unsigned int wp_grp_size        : 7;
+	unsigned int sector_size        : 7;
+	unsigned int erase_blk_en       : 1;
+	unsigned int c_size_mult        : 3;
+	unsigned int vdd_w_curr_max     : 3;
+	unsigned int vdd_w_curr_min     : 3;
+	unsigned int vdd_r_curr_max     : 3;
+	unsigned int vdd_r_curr_min     : 3;
+	unsigned int c_size             : 12;
+	unsigned int                    : 2;
+	unsigned int dsr_imp            : 1;
+	unsigned int read_blk_misalign  : 1;
+	unsigned int write_blk_misalign : 1;
+	unsigned int read_bl_partial    : 1;
+	unsigned int read_bl_len        : 4;
+	unsigned int ccc                : 12;
+	unsigned int tran_speed         : 8;
+	unsigned int nsac               : 8;
+	unsigned int taac               : 8;
+	unsigned int                    : 6;
+	unsigned int csd_structure      : 2;
+};
+
 
 #include "term.h"
 int sd_init(struct terminfo *dbg_out);
+void sd_parse_csd(int raw[4], struct csd *csd);
 
 #endif
